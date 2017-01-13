41be466f33609db30296fc29023830d4ef05dfea
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-13 21:14:30 +0000

    8 hour rain

--- methods.js
+++ methods.js
@@ -17,3 +17,17 @@ if (didItRainToday()) {
 else {
     console.log("It did not rain!");
 }
+//Is it going to rain in the next 8h?
+function didItRain8() {
+    return weatherData.hourly.data
+    .slice(0,8)
+    .some(function(hourlyData) {
+        return(hourlyData.precipType === "rain");
+    })
+}
+if (didItRain8()) {
+    console.log("It rained in the last 8 hours!");
+}
+else {
+    console.log("It did not rain in the last 8 hours!");
+}
