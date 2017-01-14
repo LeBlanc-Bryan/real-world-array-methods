bc1909dbe6b7183cf1e6f3bb4ff2b3b5d2c14b21
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-14 00:33:45 +0000

    Sunny everyday

--- methods.js
+++ methods.js
@@ -4,8 +4,8 @@ var weatherData = require("./data.json")
 //hourly is an object which contains an array called data.
 
 function didItRainToday() {
-   return weatherData.hourly.data
-   .slice(0,24)
+    return weatherData.hourly.data
+    .slice(0,24)
     .some(function(hourlyData) {
         return (hourlyData.precipType === "rain")
     })
@@ -25,6 +25,7 @@ function didItRain8() {
         return(hourlyData.precipType === "rain");
     })
 }
+
 if (didItRain8()) {
     console.log("It rained in the last 8 hours!");
 }
@@ -43,3 +44,42 @@ function gimmeTemps() {
     
 }
 console.log(gimmeTemps());
+// When is it going to rain? - Using a chain of two array methods, start with 
+// the hourly data and return an array of Date objects saying when it will rain. 
+// To do this, you'll have to: Eliminate any item where it is not raining.
+// Extract the time from the remaining items and create a Date object
+function whenitRains() {
+    return weatherData.hourly.data
+    .filter(function(hourlyData) {
+        return (hourlyData.precipType === "rain");
+    })
+        .reduce(function(dateObject, hourlyData) {
+            
+            var myDateObject = hourlyData.time;
+            dateObject.push(new Date(myDateObject * 1000));
+            return dateObject;
+        },[])
+    }
+
+console.log(whenitRains());
+
+// Is it going to be sunny all week?
+
+// Using the appropriate array method, start with the daily data 
+// and figure out whether it will be sunny every day of the week. 
+// Use the summary and check if it contains the word "sun" using your preferred method
+
+function sunnyWeek() {
+    return weatherData.daily.data
+    .every(function(dailyData) {
+        return (dailyData.summary.includes("sun"));
+    })
+}
+console.log(sunnyWeek());
+
+//How hot will it be today?
+
+// Using the appropriate array method, start with the hourly data and figure out 
+// what will be the hottest temperature today. As a hint, notice that you will need
+// to visit each item in turn, but also keep track of a separate value.
+
